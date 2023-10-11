82c306bc41ca698f48663d793a33cee1717cc3d4
Lando Ledesma <lando_ledesma@outlook.com>
2023-10-11 15:55:14 -0600

    añadiendo el github token

--- text_extractor/extractor_main.py
+++ text_extractor/extractor_main.py
@@ -4,21 +4,25 @@ import sys
 sys.path.append("..")  # añadir el directorio padre al PATH
 from utils import create_dir, load_config, remove_existing_file
 from process_directory import process_directory
+from dotenv import load_dotenv
+
+
 
 def main():
     """
     Función principal que se ejecuta cuando se inicia el script.
     """
     config = load_config()
-    github_token = os.getenv("GITHUB_TOKEN")
+    load_dotenv("../token.env")
+    GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')
 
-    if github_token is None:
+    if GITHUB_TOKEN is None:
         raise ValueError(
             "GITHUB_TOKEN no está configurado en las variables de entorno."
         )
 
     headers = {
-        "Authorization": f"Bearer {github_token}",
+        "Authorization": f"Bearer {GITHUB_TOKEN}",
         "Accept": "application/vnd.github.v3.raw",
     }
 
